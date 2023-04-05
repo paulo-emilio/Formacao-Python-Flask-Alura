220de240fde1779dde0504ee6a8e88d17178946f
Paulo Emílio <ti.pauloemilio@gmail.com>
2023-04-05 18:31:27 -0300

    03_02

--- jogoteca.py
+++ jogoteca.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template
 
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
 # Variável onde será colocada a aplicação
 app = Flask(__name__)  # __name__ faz uma referência ao próprio arquivo, ele garante que vai rodar a aplicação
 
@@ -7,10 +15,21 @@ app = Flask(__name__)  # __name__ faz uma referência ao próprio arquivo, ele g
 # Criando uma rota
 @app.route('/inicio')
 def ola():
-    lista = ['Dark Souls', 'Donkey Kong', 'Red Dead Redemption']
+    jogo1 = Jogo('Donkey Kong', 'Plataforma', 'Nintendo')
+    jogo2 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+    jogo3 = Jogo('Dark Souls', 'RPG de ação', 'Xbox 360')
+    jogo4 = Jogo('Red Dead Redemption', 'Ação e Aventura', 'Xbox 360')
+
+    lista = [jogo1, jogo2, jogo3, jogo4]
     return render_template('lista.html',
                            titulo='Jogos',
                            jogos=lista)
 
 
-app.run(host='0.0.0.0', port=8080)
+@app.route('/novo')
+def novo():
+    return render_template('novo.html',
+                           titulo='Novo Jogo')
+
+
+app.run()
