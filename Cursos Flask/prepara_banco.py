9be15c78b6d1b002595529b034f01b2c55d4129c
Paulo Emílio <ti.pauloemilio@gmail.com>
2023-04-10 20:44:08 -0300

    aula01_04

--- Cursos Flask/prepara_banco.py
+++ Cursos Flask/prepara_banco.py
@@ -6,7 +6,7 @@ try:
     conn = mysql.connector.connect(
         host='127.0.0.1',
         user='root',
-        password='root'
+        password='admin'
     )
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
